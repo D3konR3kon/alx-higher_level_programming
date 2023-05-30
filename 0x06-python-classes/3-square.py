836656305144b6fc6a190fe3e98e39e868814817
Vuyo Dila <dilavuyo@outlook.com>
2023-05-30 12:10:04 -0700

    Square attribute that returns an area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Creating Square class 
+"""
+
+class Square:
+    """
+    class Square consructor
+    """
+
+    def __init__(self, size = 0):
+
+        """ __size is private """
+
+        if isinstance(size, int) and size >= 0:
+            self.__size = size
+        elif (not isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        
+    "Area returns size * size"
+    def area(self):
+        return (self.__size * self.__size)
