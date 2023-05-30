48cd897e3d1ab0ed4d6ae7598ab8c93981ff3443
Vuyo Dila <dilavuyo@outlook.com>
2023-05-30 12:02:54 -0700

    chmodded the 2-square file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Creating Square class 
 """
@@ -7,15 +8,14 @@ class Square:
     """
     class Square consructor
     """
-    def __init__(self, size=0):
-        """
-        __size is private
-        """
-        try:
-            int(size)
-            if size >= 0:
-                self.__size = size
-            else:
-                raise ValueError('size must be >= 0')
-        except TypeError:
-            print("size must be an integer")
+
+    def __init__(self, size = 0):
+
+        """ __size is private """
+
+        if isinstance(size, int) and size >= 0:
+            self.__size = size
+        elif (not isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
