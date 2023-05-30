51fe46645d34511e46bdb4a51eee1fa4248b318b
Vuyo Dila <dilavuyo@outlook.com>
2023-05-30 11:50:37 -0700

    Create a private instance member

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Creating Square class 
+"""
+
+class Square:
+    """
+    class Square consructor
+    """
+    def __init__(self, size):
+        """
+        __size is private
+        """
+        self.__size = size
