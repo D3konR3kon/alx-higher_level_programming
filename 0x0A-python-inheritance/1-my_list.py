c8839b04f3f21fd1687ce0683d63895fc3fc7237
Vuyo Dila <dilavuyo@outlook.com>
2023-06-14 05:16:17 -0700

    my-list updated

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
+"""
+
+Class Mylist that is Ineritance form class list
+
+"""
+
+
 class MyList(list):
-    """ Class that inherits the attributes references of class list
-
-    Args:
-        list: class list
-
     """
-
+    Args:
+        * var: auxiliar variable in order to not affect the self value
+        * self: variable that refers to the object itself
+    """
     def print_sorted(self):
-        """ Method that prints the sorted list """
-        l_sorted = self.copy()
-        l_sorted.sort()
-        print(l_sorted)
+        var = []
+        var = self.copy()
+        var.sort()
+        print(var)
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
-    """ Function that returns True/False if obj is a type of a_class
-
-    Args:
-        obj: object
-        a_class: class type
-
-    Returns:
-        True if type of obj is a_class
-        False, otherwise
     """
-    return type(obj) is a_class
+    ARGS:
+        * obj: object to verify
+        * a_class: verify if this class is the class of obj
+    """
+    if type(obj) is a_class:
+        return (True)
+    return (False)
